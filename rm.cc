dd9d2ef8ca6591cea05fb9337e3c349166bbb285
Cristian KLEIN <cristiklein@gmail.com>
2013-05-20 17:01:02 +0200

    Started working on future RM

---
+++ rm.cc
@@ -0,0 +1,61 @@
+#include <arpa/inet.h>
+#include <net/if_arp.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+char *mac_ntoa(unsigned char *ptr)
+{
+	static char address[30];
+	sprintf(address, "%02X:%02X:%02X:%02X:%02X:%02X",
+		ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5]);
+	return address;
+}
+
+int main(int argc, char **argv)
+{
+	int s = socket(AF_INET, SOCK_DGRAM, 0);
+	if (s == -1) {
+		perror("Fatal error, socket() failed");
+		exit(EXIT_FAILURE);
+	}
+
+	struct sockaddr_in sa;
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(2712);
+	sa.sin_addr.s_addr = INADDR_ANY;
+	if (bind(s, (const sockaddr *)&sa, sizeof(sa)) == -1) {
+		perror("Fatal error, bind() failed");
+		exit(EXIT_FAILURE);
+	}
+
+	for (;;) { /* controller should never exit */
+		char buf[1024];
+
+		struct sockaddr_in sin;
+		socklen_t sinlen = sizeof(sin);
+		int len = recvfrom(s, buf, 1024, 0, (sockaddr*)&sin, &sinlen);
+
+		if (len > 0) {
+			buf[len] = 0;
+
+			struct arpreq arpreq;
+			arpreq.arp_flags = 0;
+			memcpy(&arpreq.arp_pa, &sin, sizeof(struct sockaddr_in));
+			strcpy(arpreq.arp_dev, "virbr0");
+			if (ioctl(s, SIOCGARP, &arpreq) == -1) {
+				perror("Error, unable to retrieve MAC address");
+			}
+
+			fprintf(stderr, "Got message from %s(%s): %s\n", inet_ntoa(sin.sin_addr), mac_ntoa((unsigned char *)&arpreq.arp_ha.sa_data), buf);
+		}
+		else {
+			perror("Error, recvfrom() failed");
+		}
+	}
+}
