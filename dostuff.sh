368b9e70bf10a987055a5b13bdaf2865c9771bb9
Cristian KLEIN <cristiklein@gmail.com>
2013-05-24 17:33:55 +0200

    New style for experiment_control logs

--- dostuff.sh
+++ dostuff.sh
@@ -1,28 +1,15 @@
-#!/bin/sh
-date +%s
-./actuator --vm rubis-hvm --cap 400
-sleep 60
+#!/bin/bash
 
-date +%s
-./actuator --vm rubis-hvm --cap 200
-sleep 60
+function setCap {
+	echo `date +%s` cap=$1
+	./actuator --vm rubis-hvm --cap $1 > /dev/null 2> /dev/null
+	sleep 60
+}
 
-date +%s
-./actuator --vm rubis-hvm --cap 100
-sleep 60
-
-date +%s
-./actuator --vm rubis-hvm --cap 50
-sleep 60
-
-date +%s
-./actuator --vm rubis-hvm --cap 100
-sleep 60
-
-date +%s
-./actuator --vm rubis-hvm --cap 200
-sleep 60
-
-date +%s
-./actuator --vm rubis-hvm --cap 400
-sleep 60
+setCap 400
+setCap 200
+setCap 100
+setCap  50
+setCap 100
+setCap 200
+setCap 400
