1701b77f468e095bcc86198c6f5a584727950983
Cristian KLEIN <cristiklein@gmail.com>
2013-06-14 10:54:16 +0200

    Added command-line options

--- rm.cc
+++ rm.cc
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <boost/program_options.hpp>
 #include <map>
 #include <net/if_arp.h>
 #include <netinet/in.h>
@@ -14,8 +15,6 @@
 #include "VirtualManager.hh"
 
 #define MAX_MESSAGE_SIZE 1024 /*< maximum number of bytes accepted through UDP */
-#define CONTROL_INTERVAL 5 /*< how often should platform be re-balanced */
-#define VIRTUAL_BRIDGE "virbr0" /*< the bridge on which VMs are connected, for IP-to-MAC translation */
 
 double inline now()
 {
@@ -26,6 +25,7 @@ double inline now()
 
 bool processMessage(int s,
 	VirtualManager &vmm,
+	const std::string virtualBridge,
 	std::map<uint32_t, std::string> &ipToVmCache,
 	std::map<std::string, double> &vmToPerformance)
 {
@@ -58,7 +58,7 @@ bool processMessage(int s,
 		struct arpreq arpreq;
 		arpreq.arp_flags = 0;
 		memcpy(&arpreq.arp_pa, &sin, sizeof(struct sockaddr_in));
-		strcpy(arpreq.arp_dev, "virbr0");
+		strcpy(arpreq.arp_dev, virtualBridge.c_str());
 		if (ioctl(s, SIOCGARP, &arpreq) == -1) {
 			perror("Error, unable to retrieve MAC address");
 			return false;
@@ -131,14 +131,38 @@ bool rebalancePlatform(VirtualManager &vmm,
 
 int main(int argc, char **argv)
 {
+	/* Parameters to program */
+	double epsilonRm;
+	double controlInterval;
+	int nCpus;
+	int listenPort;
+	std::string virtualBridge;
+
+	/* Parse command-line for epsilonRm, control interval and platform size */
+	namespace po = boost::program_options;
+	po::options_description desc("Game-theoretical resource manager for service-level-aware Cloud applications");
+	desc.add_options()
+		("help", "produce help message")
+		("port", po::value<int>(&listenPort)->default_value(2712), "UDP port to listen on for receiving matching values")
+		("epsilon", po::value<double>(&epsilonRm)->default_value(0.01), "use this value for epsilonRm; see paper for its meaning")
+		("ncpus", po::value<int>(&nCpus)->default_value(4), "maximum number of processors available for allocating to VMs")
+		("interval", po::value<double>(&controlInterval)->default_value(5), "control interval in seconds")
+		("bridge", po::value<std::string>(&virtualBridge)->default_value("virbr0"), "use this (virtual) bridge for IP-to-MAC resolution")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("help")) {
+		std::cout << desc << "\n";
+		return 1;
+	}
+
 	/* Set up some passive data structures */
 	std::map<uint32_t, std::string> ipToVmCache;
 	std::map<std::string, double> vmToPerformance;
 	std::map<std::string, double> vmToCap;
-	double epsilonRm = 0.01;
-	double platformSize = 400;
-
-	/* TODO: parse command-line for epsilonRm, control interval and platform size */
 
 	/* Class to chat with hypervisor */
 	VirtualManager vmm;
@@ -153,7 +177,7 @@ int main(int argc, char **argv)
 	/* Start listening */
 	struct sockaddr_in sa;
 	sa.sin_family = AF_INET;
-	sa.sin_port = htons(2712);
+	sa.sin_port = htons(listenPort);
 	sa.sin_addr.s_addr = INADDR_ANY;
 	if (bind(s, (const sockaddr *)&sa, sizeof(sa)) == -1) {
 		perror("Fatal error, bind() failed");
@@ -166,7 +190,7 @@ int main(int argc, char **argv)
 		/* Wait for a packet to arrive or the next control interval */
 		int ret = 0; /* as if poll() had timed out */
 		struct pollfd fds[1] = {{ s, POLLIN }};
-		int waitForMs = (CONTROL_INTERVAL - (now() - lastControl)) * 1000 + 1;
+		int waitForMs = (controlInterval - (now() - lastControl)) * 1000 + 1;
 		fprintf(stderr, "Waiting for %dms\n", waitForMs);
 		if (waitForMs > 0)
 			ret = poll(fds, 1, waitForMs);
@@ -175,11 +199,11 @@ int main(int argc, char **argv)
 
 		/* Did we receive a packet? If so, process it. */
 		if (ret > 0)
-			processMessage(s, vmm, ipToVmCache, vmToPerformance);
+			processMessage(s, vmm, virtualBridge, ipToVmCache, vmToPerformance);
 
 		/* Should we run the controller? If so, run it. */
-		if (now() - lastControl > CONTROL_INTERVAL) {
-			rebalancePlatform(vmm, platformSize, epsilonRm, vmToPerformance, vmToCap);
+		if (now() - lastControl > controlInterval) {
+			rebalancePlatform(vmm, 100 * nCpus, epsilonRm, vmToPerformance, vmToCap);
 			lastControl = now();
 		}
 	}
