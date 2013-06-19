54439ff142247c1c063aab03f4c81cc20983ec92
Cristian KLEIN <cristiklein@gmail.com>
2013-06-19 09:42:24 +0200

    Output both user-side and controller-side service level

--- prepareCsv.py
+++ prepareCsv.py
@@ -50,6 +50,7 @@ lcLogLines = open(inExpDir('lc.log')).readlines()
 
 # Extract time series
 tRubis = {}
+rRubis = {}
 sRubis = {}
 
 for line in clientLogLines:
@@ -60,6 +61,14 @@ for line in clientLogLines:
 		maxLatency = float(re.search("latency=[0-9.]+:[0-9.]+:[0-9.]+:[0-9.]+:([0-9.]+):\([0-9.]+\)", line).group(1))
 		rr = float(re.search("rr=([0-9.]+)", line).group(1))
 		tRubis[int(timestamp)] = maxLatency
+		rRubis[int(timestamp)] = rr
+	except AttributeError:
+		pass
+
+for line in lcLogLines:
+	try:
+		timestamp = float(re.search("([0-9.]+)", line).group(1))
+		rr = float(re.search("rr=([0-9.]+)", line).group(1))
 		sRubis[int(timestamp)] = rr
 	except AttributeError:
 		pass
@@ -88,14 +97,16 @@ print("# Generated using: " + ' '.join(argv), file = f)
 aggregateInterval = options.interval
 for time in range(tStart, tEnd, aggregateInterval):
 	latencies = []
+	recommandationRates = []
 	serviceLevels = []
 	for t in range(time, time + aggregateInterval):
 		try:
 			latencies.append(tRubis[t])
+			recommandationRates.append(rRubis[t])
 			serviceLevels.append(sRubis[t])
 		except KeyError:
 			print("Missing data for absolute time {0}, relative time {1}".format(t, t-tStart), file = stderr)
 			continue
 	if len(latencies) == 0: continue
 	if len(serviceLevels) == 0: continue
-	print(time-tStart, max(latencies) / 1000, avg(serviceLevels) / 100, sep = ',', file = f)
+	print(time-tStart, max(latencies) / 1000, avg(recommandationRates) / 100, avg(serviceLevels) / 100, sep = ',', file = f)
